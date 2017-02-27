71fbe43262e063cfa1748a663271c75012af0ac5
Clement Hussenot <chussenot@gmail.com>
2017-02-27 22:13:47 +0100

    Implement the proxy_handler
    - Add log management
    - Just catch other http codes than 200
    - Use requests lib (HTTP for human)
    - Add Makefile- Manage python libs with a requirements.txt file (pip)
    - Add entr scripts for watching changes and run lambda locally
    -  Use python-lambda-local lib for testing purpose
    - Add libs in ./vendor folder in global python libs PATH
    - Update the tfvars sample
    - Replace the base_api URL with the new API url and scheme
    
    @see https://pypi.python.org/pypi/python-lambda-local

--- code/buffer/entrypoint.py
+++ code/buffer/entrypoint.py
@@ -1,2 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import settings
+import json
+import logging
+import requests
+
+print('Loading entrypoint...')
+
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+def log_event(event):
+    log.debug('body: ' + str(event.get('body', '')))
+    log.debug('method: ' + str(event.get('method', '')))
+    log.debug('params: ' + str(event.get('params', '')))
+    log.debug('query: '+ str(event.get('query', '')))
+
+def query(query):
+    headers = {}
+    response = requests.get(query, headers=headers)
+    if response.status_code != 200:
+        raise Exception('Resource does not exist ' + str(response.status_code) + query )
+    return response
+
 def proxy_handler(event, context):
-    return event
+    """Call the base API with proxy params and return the response
+    """
+    log_event(event)
+    scheme = 'https://'
+    base_api = event['stageVariables']['BASE_API']
+    text = query("{0}/{1}/".format(
+        base_api,
+        event['params']['proxy'])
+    ).text.replace(base_api, scheme + event['headers']['Host'] + '/' + event['stage'])
+    return json.loads(text)
---
+++ code/buffer/settings.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+LIBS_PATH = 'vendor'
+# Appends the directory containing our libraries to the sys path.
+import sys, os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(dir_path, LIBS_PATH))
+
+# Manually load OS libraries
+# ref: https://serverlesscode.com/post/deploy-scikitlearn-on-lamba/
+import ctypes
+for d, dirs, files in os.walk(LIBS_PATH):
+    for f in files:
+        if ".so" in f:
+            ctypes.cdll.LoadLibrary(os.path.join(d, f))
