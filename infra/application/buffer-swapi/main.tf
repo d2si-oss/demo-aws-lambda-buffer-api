1fe56f9e53835702b566200b9a7dd6e7f868f9a5
Clement Hussenot <chussenot@gmail.com>
2017-02-27 20:16:01 +0100

    Create the IAM roles and polcies for the lambdas

--- infra/application/buffer-swapi/main.tf
+++ infra/application/buffer-swapi/main.tf
@@ -17,3 +17,53 @@ module "api" {
   name        = "${module.vars.fully_qualified_name}"
   description = "${var.api_description}"
 }
+
+# Roles and Policy
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "${module.vars.fully_qualified_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "logconf" {
+  name = "${module.vars.fully_qualified_name}-logconf"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+            ],
+            "Resource": [
+              "*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "logconf_to_api" {
+  name       = "${module.vars.fully_qualified_name}-logconf"
+  roles      = ["${aws_iam_role.iam_for_lambda.name}"]
+  policy_arn = "${aws_iam_policy.logconf.arn}"
+}
