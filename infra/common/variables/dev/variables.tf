a43b52feefc5be8ef125c5aebc90c0bfbd4e2899
Clement Hussenot <chussenot@gmail.com>
2017-02-27 19:33:48 +0100

    Setup some variables for a `dev` environment

---
+++ infra/common/variables/dev/variables.tf
@@ -0,0 +1,16 @@
+# Global Environment Variables
+
+variable "allowed_account_id" {
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "application" {
+  default = "buffer"
+}
