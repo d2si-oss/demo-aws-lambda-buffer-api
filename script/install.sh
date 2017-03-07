30de81ae9a64c8d1c26a80ba9db9cd145d25148c
Clement Hussenot <chussenot@gmail.com>
2017-03-07 22:38:33 +0100

    Install the vendor dependecies for the CI

--- script/install.sh
+++ script/install.sh
@@ -6,3 +6,5 @@ set -x
 pip install -r ci-requirements.txt
 apt-get install -y curl tar
 curl https://raw.githubusercontent.com/apex/apex/master/install.sh | sh
+cd code/buffer/functions/proxy
+make install_deps
